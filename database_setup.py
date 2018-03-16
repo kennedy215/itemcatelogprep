e18004e8c6bd659d0770224aa26d5591a99e9783
Kennedy DeSousa <kennedydesousa@gmail.com>
2018-03-16 10:45:15 -0700

    added jsonif

--- database_setup.py
+++ database_setup.py
@@ -13,6 +13,13 @@ class Restaurant(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    
+    @property
+    def serialize(self):
+        return{
+            'id' : self.id,
+            'name' : self.name,
+        }
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
@@ -24,6 +31,19 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+
+    @property
+    def serialize(self):
+        return{
+        'id' : self.id,
+        'name' : self.name,
+        'description' : self.description,
+        'price' : self.price,
+        'course' : self.course,
+        }
+
+
+
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
--- project.py
+++ project.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, url_for, redirect, request, flash
+from flask import Flask, render_template, url_for, redirect, request, flash, jsonify
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
-# app.secret_key = "kennedysecret"
+app.secret_key = "kennedysecret"
+
 engine = create_engine('sqlite:///restaurantmenu.db')
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
 
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
@@ -29,8 +31,15 @@ def restaurantMenu(restaurant_id):
     return render_template("index.html", restaurant=restaurant, items=item)
 
 
-# Task 1: Create route for newMenuItem function here
+# Export Data and parse into JSON
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems = [i.serialize for i in items])
 
+
+# Task 1: Create route for newMenuItem function here
 @app.route('/restaurants/<int:restaurant_id>/new', methods = ['GET','POST'])
 def newMenuItem(restaurant_id):
     if request.method == 'POST':
@@ -41,6 +50,7 @@ def newMenuItem(restaurant_id):
         restaurant_id=restaurant_id)
         session.add(newItem)
         session.commit()
+        flash("You've successfully created a new restaurant!!!")
         return  redirect(url_for("restaurantMenu", restaurant_id=restaurant_id))
     else:
         # flash("Please type your name!")
@@ -63,6 +73,7 @@ def editMenuItem(restaurant_id, menu_id):
             editedItem.course = request.form['course']
         session.commit()
         # return url_for("restaurantMenu", restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
+        flash("Restaurant was edited successfully!")
         return redirect(url_for("restaurantMenu", restaurant_id=restaurant_id, menu_id=menu_id))
     else:
             # return "page to edit a menu item. Task 2 complete!"
@@ -76,6 +87,7 @@ def deleteMenuItem(restaurant_id, menu_id):
     if request.method == "POST":
         session.delete(deleteItem)
         session.commit()
+        flash("Item was deleted!")
         return redirect(url_for("restaurantMenu", restaurant_id=restaurant_id))
     else:
         return render_template('deletemenuitem.html', item=deleteItem)
